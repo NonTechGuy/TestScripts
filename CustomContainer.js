30269a0d63673855799c5716d3e0299a798cf4cc
Craig Bartlett <craigmbartlett@gmail.com>
2014-06-27 15:38:16 +0200

    no message

--- CustomContainer.js
+++ CustomContainer.js
@@ -6,16 +6,16 @@ function CustomContainer(label, color, w, h) {
     this.initialize();
 }
 //Inheritance from Container
-Item.prototype = new createjs.Container(); 
-Item.prototype.Container_initialize = Item.prototype.initialize; 
+CustomContainer.prototype = new createjs.Container(); 
+CustomContainer.prototype.Container_initialize = CustomContainer.prototype.initialize; 
      
-Item.prototype.initialize = function () {
+CustomContainer.prototype.initialize = function () {
     this.Container_initialize();
     
     var shape = new createjs.Shape()
         shape.graphics.beginFill(this._color).drawRect(0, 0, this._width, this._height);
         shape.x = xpos;
-        shape.name = "item"+j
+        shape.name = j;
             
     var txt = new createjs.Text();
         txt.text = this._label;
