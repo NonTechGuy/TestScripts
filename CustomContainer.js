14238c51f57baf23d52e7f1e5c609aec2fe4f2d8
Craig Bartlett <craigmbartlett@gmail.com>
2014-07-02 11:07:40 +0200

    Commented out shape

--- CustomContainer.js
+++ CustomContainer.js
@@ -12,13 +12,13 @@ CustomContainer.prototype.Container_initialize = CustomContainer.prototype.initi
 CustomContainer.prototype.initialize = function () {
     this.Container_initialize();
     
-    var shape = new createjs.Shape()
-        shape.graphics.beginFill(this._color).drawRect(0, 0, this._width, this._height);
+    /*var shape = new createjs.Shape()
+        shape.graphics.beginFill(this._color).drawRect(0, 0, this._width, this._height);*/
     
     var bg = new createjs.Image();
         bg.src = "images/green.jpg";
             
     var txt = new createjs.Text(this._label, "20px Arial", "#ffffff");
            
-    this.addChild(shape, txt, bg);
+    this.addChild(/*shape,*/  bg,txt);
 }
